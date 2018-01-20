c040c29a865fae4b8d912d4a0144f32a6abe01da
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-01-20 19:31:10 +0900

    added error handling to express

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+function errorHandler(err, req, res, next) {
+  console.error(err.message);
+  console.error(err.stack);
+}
+
 app.get("/show", (req, res) => {
   var random_boolean = Math.random() >= 0.5;
   res.send(random_boolean);
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
